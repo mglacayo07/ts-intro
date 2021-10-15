6224baa8e4ac904fffa84e63eb743f4a0fee0a72
Maria Lacayo <mg.lacayo07@gmail.com>
2021-10-14 19:15:43 -0500

    ejercicio 1. tipos básicos

---
+++ src/ejercicios/t01-tipos.basicos.ts
@@ -0,0 +1,18 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+let myName: string = "Richie";
+myName = "Maria";
+console.log(myName);
+
+let myAge: number = 28;
+console.log(myAge);
+
+let hp: number | string = 95;
+console.log(hp);
+hp = "FULL";
+console.log(hp);
+
+let alive: boolean = true;
+console.log(alive);
